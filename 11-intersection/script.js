8780f71290545b0dba5b3f2e64e6a9c3d176ae0f
Vadim Savinov <vasaykh@yandex.ru>
2022-08-21 23:09:45 +0500

    feat: добавить решение

--- 11-intersection/script.js
+++ 11-intersection/script.js
@@ -4,11 +4,23 @@
  * Напишите функцию intersection(arr1, arr2). Она должна принимать
  * на вход два массива целых чисел. Функция должна вернуть новый
  * массив чисел, содержащихся в обоих исходных массивах.
- * 
-*/
+ *
+ */
 
 function intersection(arr1, arr2) {
-    // Напишите код здесь
+  const arr3 = [];
+  const arr4 = [];
+  arr1.forEach((el) => {
+    if (arr2.includes(el)) {
+      arr3.push(el);
+    }
+  });
+  arr3.forEach((el, i) => {
+    if (i === arr3.join('').indexOf(el)) {
+      arr4.push(el);
+    }
+  });
+  return arr4;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
