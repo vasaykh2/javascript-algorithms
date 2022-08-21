72cfb9b54fc1dec0ac4ecfa54b39e715a3fadaed
Vadim Savinov <vasaykh@yandex.ru>
2022-08-21 21:33:08 +0500

    feat: добавить решение

--- 08-fibonacci/script.js
+++ 08-fibonacci/script.js
@@ -9,7 +9,12 @@
 */
 
 function fibonacci(n) {
-    // Напишите код здесь
+const numF = [];
+numF[0] = 0;
+numF[1] = 1;
+   for (i = 2; i <= n; i++)
+    numF[i] = numF[i - 2] + numF[i - 1];
+    return numF[numF.length - 2];
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
