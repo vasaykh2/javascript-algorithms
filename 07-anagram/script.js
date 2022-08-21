c0f7b30cc9aaee897bece070395164164add900e
Vadim Savinov <vasaykh@yandex.ru>
2022-08-21 19:08:38 +0500

    feat: добавить решение

--- 07-anagram/script.js
+++ 07-anagram/script.js
@@ -5,11 +5,19 @@
  * Напишите функцию, проверяющую, являются ли две строки анаграммами друг друга
  * (регистр букв не имеет значения). Для простоты примите, что в этих строках
  * нет пробелов и знаков препинания.
- * 
-*/
+ *
+ */
 
 function anagram(str1, str2) {
-    // Напишите код здесь
+  const arrStr1 = str1.toLowerCase().split('');
+  const arrStr2 = str2.toLowerCase().split('');
+  return arrStr1.reduce((res, current) => {
+    return (
+      str2.toLowerCase().includes(current) &&
+      str1.length === str2.length &&
+      str1.toLowerCase() !== str2.toLowerCase()
+    );
+  });
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
